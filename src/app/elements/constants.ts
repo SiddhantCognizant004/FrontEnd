a1ea2c81dc03466c6c1c38266444e5b47169092a
SiddhantCognizant004 <SiddhantPandurang.Phatangare@cognizant.com>
2026-05-06 21:56:15 +0530

    Delete src directory

--- src/app/elements/constants.ts
+++
@@ -1,9 +0,0 @@
-export const API_URL = 'http://localhost:8090/';
-
-export const TOKEN_KEY = 'agrichain_token';
-export const LOGIN_INFO = "logged_in_info";
-
-export const ADMIN_DASHBOARD = '/dashboard/admin/';
-
-export const USER_PATH = "users"
-export const REPORT_PATH = "api/report";
